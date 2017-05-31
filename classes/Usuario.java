ad74742ffc3f3e9d173b8940270761e51ad3e849
Danilo <danva.comp@gmail.com>
2017-05-31 16:01:10 -0300

    Class user

---
+++ classes/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario{
+	
+	private int lastSeen;
+	private int friendRequest = 0;
+	private int newMessagens = 0;
+	private String name;
+	private String email;
+	private String senha;
+	private int firstMonthEntered;
+	private int firstYearEntered;
+
+	
+	public void getFirstMonthEntered(){
+		return this.firstMonthEntered;
+	}
+
+}
