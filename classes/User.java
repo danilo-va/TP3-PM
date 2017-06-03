25ee96b5112a1506dfe3e5ae7675b9d77fa7ce43
daniloazevedo <danva.comp@gmail.com>
2017-06-03 09:56:23 -0300

    Some changes

--- classes/User.java
+++ classes/User.java
@@ -1,13 +1,16 @@
-public class Usuario{
+public class User{
 	
-	private int lastSeen;
+	private String lastSeen;
 	private int friendRequest = 0;
 	private int newMessagens = 0;
 	private String name;
 	private String email;
-	private String senha;
+	private String password;
 	private int firstMonthEntered;
 	private int firstYearEntered;
+	private String email;
+	// private photo
+	private int numFriends;
 
 	
 	public void getFirstMonthEntered(){
